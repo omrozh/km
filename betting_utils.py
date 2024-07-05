565be0fcab719d6f00933d925e102f141046a256
omrozh <53612475+omrozh@users.noreply.github.com>
2024-07-05 23:30:10 +0300

    Update betting_utils.py

--- betting_utils.py
+++ betting_utils.py
@@ -12,6 +12,12 @@ api_key = "na"
 def get_live_score(open_bet):
     r = requests.get("https://www.thesportsdb.com/api/v2/json/623577/livescore.php?s=Soccer")
     match_likelihood = 0
+    if not r.json.get("events"):
+        return {
+            "intHomeScore": "-",
+            "intAwayScore": "-",
+            "strProgress": "N/A"
+        }
     for i in r.json().get("events"):
         for c in open_bet.team_1.split(" "):
             if c in i.get("strHomeTeam"):
