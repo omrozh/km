727efa669814170ead02efb76bf1d89338bfdb51
omrozh <53612475+omrozh@users.noreply.github.com>
2024-07-06 14:13:11 +0300

    h

--- casino_utils.py
+++ casino_utils.py
@@ -48,7 +48,7 @@ def check_sign(request):
     print(sorted_data)
     data_json = json.dumps(sorted_data)
     print(data_json)
-    print(hashlib.sha256((data_json + hash_authorization_key).encode()))
+    print(hashlib.sha256((data_json + hash_authorization_key).encode()).hexdigest())
     print((data_json + hash_authorization_key).encode())
 
     hash_auth_local = hashlib.sha256((data_json + hash_authorization_key).encode()).hexdigest()  # Hashing of data
